c2372f723557e65e923523f7427bf98b8a12dc48
Matthieu Mouminoux <mmouminoux@mirakl.com>
2020-02-01 11:04:24 +0100

    Add version 2.4.x

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gocolly/colly"
 	"github.com/pkg/errors"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -35,14 +36,105 @@ func main() {
 		"21":   "2.1.X",
 		"22":   "2.2.X",
 		"23":   "2.3.X",
+		"24":   "2.4.X",
 	}
 
-	for k, v := range kafkaVersion {
-		getDoc(k, v)
+	for k, versionLabel := range kafkaVersion {
+		var properties []property
+		if docVersion, _ := strconv.Atoi(k); docVersion < 24 || docVersion > 100 {
+			properties = getDocBefore24(k)
+		} else {
+			properties = getDoc(k)
+		}
+
+		bytes, err := json.Marshal(properties)
+		if err != nil {
+			panic(err)
+		}
+		f, err := os.Create("kafka-doc-" + versionLabel + ".json")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		w := bufio.NewWriter(f)
+		_, err = w.Write(bytes)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
-func getDoc(versionCode string, versionLabel string) {
+func getDoc(versionCode string) []property {
+	var properties []property
+	c := colly.NewCollector()
+
+	c.OnHTML(`#configuration-template`, func(e *colly.HTMLElement) {
+		document, err := goquery.NewDocumentFromReader(strings.NewReader(e.Text))
+		if err != nil {
+			panic(err)
+		}
+
+		var currentCategory string
+
+		document.Find(".config-list > li, h3 > a").Each(func(i int, selection *goquery.Selection) {
+			p := property{}
+
+			if selection.Nodes[0].Data == "a" {
+				categoryLinkAttr, _ := selection.Attr("id")
+				currentCategory = strings.Replace(categoryLinkAttr, "configs", "", 1)
+				return
+			}
+
+			p.Category = currentCategory
+
+			propKey := selection.Find("b").First()
+			p.Name = propKey.Text()
+			html, _ := selection.Html()
+
+			description := strings.Split(strings.Split(html, "</b>: ")[1], "<br/>")[0]
+			p.Description = description
+
+			selection.Find(".horizontal-list").Each(func(i int, selection *goquery.Selection) {
+				selection.Find("li").Each(func(i int, selection *goquery.Selection) {
+					html, _ := selection.Html()
+
+					prop := selection.Find("b").First().Text()
+					value := strings.Split(html, "</b>: ")[1]
+
+					switch prop {
+					case "Type":
+						p.Type = value
+					case "Default":
+						p.Default = value
+					case "Valid Values":
+						p.ValidValues = value
+					case "Importance":
+						p.Importance = value
+					case "Update Mode":
+						p.DynamicUpdateMode = value
+					case "Server Default Property":
+						p.ServerDefaultProperty = value
+					}
+				})
+			})
+			properties = append(properties, p)
+		})
+	})
+	c.OnError(func(response *colly.Response, e error) {
+		if response.StatusCode == 404 {
+			panic(errors.Errorf("Not found"))
+		}
+		panic(errors.Wrapf(e, "colly error (status:%d)", response.StatusCode))
+	})
+	err := c.Visit("https://kafka.apache.org/" + versionCode + "/documentation.html")
+	if err != nil {
+		panic(err)
+	}
+
+	return properties
+}
+
+func getDocBefore24(versionCode string) []property {
 	var properties []property
 	c := colly.NewCollector()
 	c.OnHTML(`#configuration-template`, func(e *colly.HTMLElement) {
@@ -121,18 +213,6 @@ func getDoc(versionCode string, versionLabel string) {
 	if err != nil {
 		panic(err)
 	}
-	bytes, err := json.Marshal(properties)
-	if err != nil {
-		panic(err)
-	}
-	f, err := os.Create("kafka-doc-" + versionLabel + ".json")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
-	_, err = w.Write(bytes)
-	if err != nil {
-		panic(err)
-	}
+
+	return properties
 }
